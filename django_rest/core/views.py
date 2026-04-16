92c86c351b24bb9a8aa81992333780037c3cfa0d
Sourav <SouravBhardwaj997@gmail.com>
2026-04-17 00:15:09 +0530

    Some changes

--- django_rest/core/views.py
+++ django_rest/core/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-# Create your views here.
 
+from .models import Student
+from rest_framework.response import Response
+from .serializer import StudentSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+# Create your views here.
+@api_view(["GET"])
 def students(request):
-    return JsonResponse({"id":1,"name":"Soruav","age":2})
+    if request.method == "GET":
+       students = Student.objects.all()
+       serializer =StudentSerializer(students,many=True)
+       return Response(serializer.data,status=status.HTTP_200_OK)
