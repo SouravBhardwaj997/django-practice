92ef894d99a508edbd318af8e2bf63907557c36a
Sourav <SouravBhardwaj997@gmail.com>
2026-04-16 23:11:40 +0530

    Send Json Response

--- django_rest/core/views.py
+++ django_rest/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
+
+def students(request):
+    return JsonResponse({"id":1,"name":"Soruav","age":2})
