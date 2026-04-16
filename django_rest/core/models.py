712217460e629381f98f8e950a5c9bfab3376a83
Sourav <SouravBhardwaj997@gmail.com>
2026-04-16 23:20:13 +0530

    Add Students and Teachers Models

--- django_rest/core/models.py
+++ django_rest/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    stu_id = models.CharField(max_length=10)
+    name=models.CharField(max_length=50)
+    dept= models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+
+class Teachers(models.Model):
+    emp_id= models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    dept = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
