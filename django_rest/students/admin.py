c72a5075b7163e55aeb2eab01a0f181300f8912b
Sourav <SouravBhardwaj997@gmail.com>
2026-04-19 15:51:03 +0530

    added products products review and stores model

--- django_rest/students/admin.py
+++ django_rest/students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product,ProductReview,Stores
+
+admin.site.register(Product)
+admin.site.register(ProductReview)
+admin.site.register(Stores)
--- django_rest/students/models.py
+++ django_rest/students/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class ProductReview(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    review=models.TextField()
+    reviewer=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class Stores(models.Model):
+    name=models.CharField(max_length=20)
+    location=models.CharField(max_length=10)
+    products=models.ManyToManyField(Product,related_name="stores")
