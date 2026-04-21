cf28a7590b1047cecb91a5b7830e6ff8c9239519
Sourav <SouravBhardwaj997@gmail.com>
2026-04-21 23:22:34 +0530

    add delete endpoint

--- django_rest/core/views.py
+++ django_rest/core/views.py
@@ -121,3 +121,9 @@ class Teachers_details(APIView):
          serializer.save()
          return Response(serializer.data,status=status.HTTP_200_OK)
       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+   
+   def delete(self,request,teacher_id):
+      teacher = self.get_object(teacher_id)
+      serializer = teacher.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+
