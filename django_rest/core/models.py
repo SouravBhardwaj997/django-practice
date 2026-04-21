05c2a6b323b44868793ead19ac39bb86eab3b81e
Sourav <SouravBhardwaj997@gmail.com>
2026-04-21 22:36:17 +0530

    added new migration

--- django_rest/core/models.py
+++ django_rest/core/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 
 class Student(models.Model):
     name=models.CharField(max_length=100)
-    email=models.CharField(max_length=100,unique=True)
-    age=models.IntegerField()
-    course=models.CharField(max_length=100)
+    email=models.CharField(max_length=100,unique=True,null=True)
+    age=models.IntegerField(null=True, blank=True)
+    course=models.CharField(max_length=100,null=True)
     created_at=models.DateTimeField(default=timezone.now())
     def __str__(self):
         return self.name
